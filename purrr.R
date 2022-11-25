e07a53b467f5c950e748dfdd93306b51e05c326a
George Mwangi <mwangigeorge648@gmail.com>
2022-11-25 16:37:44 +0300

    changes

--- purrr.R
+++ purrr.R
@@ -1,6 +1,6 @@
 install.packages("repurrrsive")
 library(repurrrsive)
-wesanderson
+
 
 data("wesanderson")
 
@@ -164,7 +164,187 @@ car_prices %>%
   )
 
 
+data("sw_vehicles")
+
+head(sw_vehicles)
+
+str(sw_vehicles)
+as.data.frame(sw_vehicles[[1]]) %>% 
+  select(1:6) %>% 
+  slice_head() %>% 
+  pivot_longer(
+    everything(),
+    names_to = "aspects",
+    values_to = "info"
+  )
+
+
+list(1, "I can", 10, 0, "purrr") %>%
+  map(safely(log, otherwise = NA_real_)) %>%
+  # Transpose the result
+  transpose()
 
 
 
+map(sw_vehicles, "length") %>% 
+  map(function(x){
+    ifelse(x == "unknown", NA,
+           as.numeric(x))
+  })
 
+
+# Map over sw_people and pull out the height element
+height_ft <- map(sw_people, "height") %>% 
+  map(safely(function(x){
+    x * 0.0328084
+    #set quiet to F so that errors are printed
+  }, quiet = F)) %>% 
+  transpose()
+
+# Print your list, the result element, and the error element
+height_ft
+height_ft[["result"]]
+height_ft[["error"]]
+
+
+# Take the log of each element in the list
+list(1, "I can", 10, 0, "purrr") %>% 
+  map(possibly(function(x){
+    log(x)
+  }, otherwise  = NA_real_))
+
+sw_people[[87]]["height"]
+
+sw_people %>% 
+  map("height") %>% 
+  map(function(x){
+    ifelse(x == "unknown", NA, as.numeric(x))
+  })
+
+
+sw_people %>%
+  # retrieve all heights from the list
+  map("height") %>%
+  # change unknown to NA
+  map(function(x) {
+    ifelse(x == "unknown", NA, as.numeric(x))
+  }) %>%
+  # multiply each element by 2
+  map(possibly(function(x) {
+    x * 2
+  }, otherwise = NA_real_))
+
+
+# Print with walk
+walk(people_by_film, print)
+head(walk(sw_people, print))
+
+library(gapminder)
+
+gapminder %>% 
+  group_split(country) %>% 
+  map(~ggplot(
+    data = ., aes(year, lifeExp)
+  )+
+    geom_point()
+  )
+
+
+# Load the gap_split data
+data(gap_split)
+
+# Map over the first 10 elements of gap_split
+plots <- map2(gap_split[1:10], 
+              names(gap_split[1:10]), 
+              ~ ggplot(.x, aes(year, lifeExp)) + 
+                geom_line() +
+                labs(title = .y))
+
+# Object name, then function name
+walk(plots, print)
+
+
+
+# Load the data
+data(gh_users)
+
+# Check if data has names
+names(gh_users)
+
+# Map over name element of list
+map(gh_users, ~.x[["name"]])
+
+
+# Name gh_users with the names of the users
+gh_users_named <- gh_users %>% 
+  set_names(map_chr(gh_users, "name"))
+
+# Check gh_repos structure
+str(gh_repos)
+
+# Name gh_repos with the names of the repo owner
+gh_repos_named <- gh_repos %>% 
+  map_chr(~ .[[1]]$owner$login) %>% 
+  set_names(gh_repos, .)
+
+
+# Determine who joined github first
+map_chr(gh_users, ~.x[["created_at"]]) %>%
+  set_names(map_chr(gh_users, "name")) %>%
+  sort()
+
+# Determine user versus organization
+map_lgl(gh_users, ~.x[["type"]] == "User") 
+
+# Determine who has the most public repositories
+map_int(gh_users, ~.x[["public_repos"]]) %>%
+  set_names(map_chr(gh_users, "name")) %>%
+  sort()
+
+
+
+# Map over gh_repos to generate numeric output
+map(gh_repos,
+    ~map_dbl(.x, 
+             ~.x[["size"]])) %>%
+  # Grab the largest element
+  map_dbl(~max(.x))
+
+names(sw_people)
+
+glimpse(sw_people)
+
+
+# create  a dataframe
+sw_people %>% 
+  map_df(`[`, c("name", "height"))
+
+
+# Create a data frame with four columns
+map_df(gh_users, `[`, 
+       c("login", "name", "followers","public_repos")) %>%
+  # Plot followers by public_repos
+  ggplot(., 
+         aes(x = followers, y = public_repos)) + 
+  # Create scatter plots
+  geom_point()
+
+
+
+# Turn data into correct data frame format
+film_by_character <- tibble(filmtitle = map_chr(sw_films, "title")) %>%
+  mutate(filmtitle, characters = map(sw_films, "characters")) %>%
+  unnest(cols = c(characters))
+
+# Pull out elements from sw_people
+sw_characters <- map_df(sw_people, `[`, c("height","mass","name","url"))
+
+# Join our two new objects
+character_data <- inner_join(film_by_character, sw_characters, by = c("characters" = "url")) %>%
+  # Make sure the columns are numbers
+  mutate(height = as.numeric(height), mass = as.numeric(mass))
+
+# Plot the heights, faceted by film title
+ggplot(character_data, aes(x = height)) +
+  geom_histogram(stat = "count") +
+  facet_wrap(~ filmtitle)
